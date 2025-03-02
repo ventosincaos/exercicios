e5d6edb82f4c55a5aeb4c407c233032cf791d55f
— mia <111576277+ventosincaos@users.noreply.github.com>
2025-03-02 12:44:48 -0300

    Update desafio1.js

--- basicos/desafio1.js
+++ basicos/desafio1.js
@@ -11,26 +11,26 @@ const MENSAGEM_NIVEL = "está no nível de";
 // início
 // estrutura decisão para XP do héroi
 switch (true) {
-  case experienciaHeroi >= 1001 && experienciaHeroi <= 2000:
-    nivelHeroi = nivel[1];
+    case experienciaHeroi >= 10001:
+    nivelHeroi = nivel[7];
     break;
-  case experienciaHeroi >= 2001 && experienciaHeroi <= 5000:
-    nivelHeroi = nivel[2];
-    break;
-  case experienciaHeroi >= 5001 && experienciaHeroi <= 7000:
-    nivelHeroi = nivel[3];
-    break;
-  case experienciaHeroi >= 7001 && experienciaHeroi <= 8000:
-    nivelHeroi = nivel[4];
-    break;
-  case experienciaHeroi >= 8001 && experienciaHeroi <= 9000:
-    nivelHeroi = nivel[5];
-    break;
-  case experienciaHeroi >= 9001 && experienciaHeroi <= 10000:
+    case experienciaHeroi >= 9001:
     nivelHeroi = nivel[6];
     break;
-  case experienciaHeroi >= 10001:
-    nivelHeroi = nivel[7];
+  case experienciaHeroi >= 8001:
+    nivelHeroi = nivel[5];
+    break;
+    case experienciaHeroi >= 7001:
+    nivelHeroi = nivel[4];
+    break;
+    case experienciaHeroi >= 5001:
+    nivelHeroi = nivel[3];
+    break;
+    case experienciaHeroi >= 2001:
+    nivelHeroi = nivel[2];
+    break;
+  case experienciaHeroi >= 1001:
+    nivelHeroi = nivel[1];
     break;
   default:
     nivelHeroi = nivel[0];
