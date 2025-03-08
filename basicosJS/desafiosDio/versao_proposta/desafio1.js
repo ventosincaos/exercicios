af46bf8611b9c7825c9ee8c718f1c14d65fc0152
ventosincaos <111576277+ventosincaos@users.noreply.github.com>
2025-03-08 02:32:16 -0300

    Update desafio1.js

--- basicosJS/desafiosDio/versao_proposta/desafio1.js
+++ basicosJS/desafiosDio/versao_proposta/desafio1.js
@@ -1,14 +1,14 @@
 // CONSTANTS
-const categories = require('../json/exp.json');
+const level = require('../json/exp.json');
 const MESSAGE_NAME_PREFIX = "O héroi de nome";
 const MESSAGE_LEVEL_SUFFIX = "está no nível de";
 
 // START
 // HERO EXP SEARCH FUNCTION 
 function gameExperience(heroName, heroExperience) {
-  for (let i = 0; i < categories.levelExperience.length; i++) {
-    if (heroExperience >= categories.levelExperience[i].minExperience && heroExperience <= categories.levelExperience[i].maxExperience){
-      return `${MESSAGE_NAME_PREFIX} ${heroName} ${MESSAGE_LEVEL_SUFFIX} ${categories.levelExperience[i].name}`;
+  for (let categories of level.levelExperience) {
+    if (heroExperience >= categories.minExperience && heroExperience <= categories.maxExperience){
+      return `${MESSAGE_NAME_PREFIX} ${heroName} ${MESSAGE_LEVEL_SUFFIX} ${categories.name}`;
     }
   }
 } //END HERO EXP SEARCH FUNCTION
