086c6ae189d48a204994e1ab31886a826d1a9744
ventosincaos <111576277+ventosincaos@users.noreply.github.com>
2025-03-09 23:46:12 -0300

    Updade - Desafio2
    Criação de Json e Melhoria de código

--- basicosJS/desafiosDio/versao_proposta/desafio1.js
+++ basicosJS/desafiosDio/versao_proposta/desafio1.js
@@ -1,12 +1,12 @@
 // CONSTANTS
-const level = require('../json/exp.json');
+const allExperience = require('../json/exp.json');
 const MESSAGE_NAME_PREFIX = "O héroi de nome";
 const MESSAGE_LEVEL_SUFFIX = "está no nível de";
 
 // START
 // HERO EXP SEARCH FUNCTION 
 function gameExperience(heroName, heroExperience) {
-  for (let categories of level.levelExperience) {
+  for (let categories of allExperience.levelExperience) {
     if (heroExperience >= categories.minExperience && heroExperience <= categories.maxExperience){
       return `${MESSAGE_NAME_PREFIX} ${heroName} ${MESSAGE_LEVEL_SUFFIX} ${categories.name}`;
     }
