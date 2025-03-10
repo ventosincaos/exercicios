4288ec56f06c320f81c1b7382230a7a8b0d57596
ventosincaos <111576277+ventosincaos@users.noreply.github.com>
2025-03-10 00:02:44 -0300

    Update: apis

--- basicosJS/desafiosDio/versao_api/apiDesafio1.js
+++ basicosJS/desafiosDio/versao_api/apiDesafio1.js
@@ -3,34 +3,19 @@ const express = require("express");
 const app = express();
 const port = 3001;
 
-// START
-app.use(express.json());
-
-// VARIABLES
-let levelExperience = [
-  ["ferro", -Infinity, 1000],
-  ["bronze", 1001, 2000],
-  ["prata", 2001, 5000],
-  ["ouro", 5001, 7000],
-  ["platina", 7001, 8000],
-  ["ascendente", 8001, 9000],
-  ["imortal", 9001, 10000],
-  ["radiante", 10001, Math.pow(10, 9)],
-];
-
-// CONSTANTS MESSAGE
+// CONSTANTS 
+const allExperience = require('../json/exp.json')
 const MESSAGE_NAME_PREFIX = "O héroi de nome";
 const MESSAGE_LEVEL_SUFFIX = "está no nível de";
 
-// HERO EXP SEARCH FUNCTION
-function gameExperience(heroName, heroExperience) {
-  for (let i = 0; i < levelExperience.length; i++) {
-    let nameLevelExperience = levelExperience[i][0];
-    let minExperience = levelExperience[i][1];
-    let maxExperience = levelExperience[i][2];
+// START
+app.use(express.json());
 
-    if (heroExperience >= minExperience && heroExperience <= maxExperience) {
-      return `${MESSAGE_NAME_PREFIX} ${heroName} ${MESSAGE_LEVEL_SUFFIX} ${nameLevelExperience}`;
+// HERO EXP SEARCH FUNCTION 
+function gameExperience(heroName, heroExperience) {
+  for (let categories of allExperience.levelExperience) {
+    if (heroExperience >= categories.minExperience && heroExperience <= categories.maxExperience){
+      return `${MESSAGE_NAME_PREFIX} ${heroName} ${MESSAGE_LEVEL_SUFFIX} ${categories.name}`;
     }
   }
 } //END HERO EXP SEARCH FUNCTION
--- basicosJS/desafiosDio/versao_api/apiDesafio2.js
+++ basicosJS/desafiosDio/versao_api/apiDesafio2.js
@@ -2,35 +2,21 @@
 const express = require("express");
 const app = express();
 const port = 3002;
-// START
-app.use(express.json());
-
-//VARIABLES
-var level = [
-  ["ferro", -Infinity, 10],
-  ["bronze", 11, 20],
-  ["prata", 21, 50],
-  ["ouro", 51, 80],
-  ["diamante", 81, 90],
-  ["lendário", 91, 100],
-  ["imortal", 101, Math.pow(10, 9)],
-];
 
 // CONSTANTS
+const allStatus = require('../json/status.json');
 const MESSAGE_NAME_PREFIX = "O héroi de nome";
 const MESSAGE_LEVEL_SUFFIX = "está no nível de";
 
+// START
+app.use(express.json());
+
 // HERO LEVEL SEARCH FUNCTION
 function gameStatus(victory, defeat, heroName) {
-  let result = victory - defeat;
-
-  for (let i = 0; i < level.length; i++) {
-    let levelName = level[i][0];
-    let minLevel = level[i][1];
-    let maxLevel = level[i][2];
-
-    if (result >= minLevel && result <= maxLevel) {
-      return `${MESSAGE_NAME_PREFIX} ${heroName} tem de saldo de ${victory} vitórias ${MESSAGE_LEVEL_SUFFIX} **${levelName}**`;
+  wins = victory - defeat;
+  for(let status of allStatus.levelStatus) {
+    if (wins >= status.minWins && wins <= status.maxWins){
+      return `${MESSAGE_NAME_PREFIX} ${heroName} tem de saldo de ${victory} vitórias ${MESSAGE_LEVEL_SUFFIX} **${status.name}**`;
     }
   }
 } // END FUNCTION
