28c5a42b91e595e0d9093a06d7f7874594fee0f9
ventosincaos <111576277+ventosincaos@users.noreply.github.com>
2025-03-10 03:17:21 -0300

    update: comentários

--- basicosJS/desafiosDio/versao_proposta/desafio2.js
+++ basicosJS/desafiosDio/versao_proposta/desafio2.js
@@ -1,11 +1,11 @@
 // CONSTANTS
-// antes status era um array, agora está em .json
+// antes status era um array e depois, foi alterado para uma matriz, agora está em .json
 const allStatus = require('../json/status.json');
 const MESSAGE_NAME_PREFIX = "O héroi de nome";
 const MESSAGE_LEVEL_SUFFIX = "está no nível de";
 
 // START
-// em primeira versão, foi usado comparadores no for, mas foi alterado para for ... of
+// em primeira versão, foram utilizados comparadores no for, mas foi alterado para for ... of
 function gameStatus(victory, defeat, heroName) {
   wins = victory - defeat;
   for(let status of allStatus.levelStatus) {
